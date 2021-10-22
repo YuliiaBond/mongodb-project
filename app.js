570a063b8896348a6d1b3d1aa08d79a906c06a84
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-10-23 00:04:19 +0300

    1

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 /* "start": "node app",
     "start:dev": "nodemon app" */
 
-// Задача: просто подключиться к базе
 const mongoose = require('mongoose');
 
 const DB_HOST = 'mongodb+srv://Yuliia:ZapZv9lN4SyGKcrM@cluster0.cfqno.mongodb.net/phonebook?retryWrites=true&w=majority';
