9a0d841b5ae009c69737a566a8ea969acb8298ac
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-10-23 00:49:56 +0300

    env

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // npm init -y
 // yarn add -D nodemon
 // yarn add mongoose
+// yarn add dotenv
 
 /* "start": "node app",
     "start:dev": "nodemon app" */
 
 const mongoose = require('mongoose');
 
-const { DB_HOST } = require('./config');
+// const dotenv = require('dotenv');
+// dotenv.config();
+require ('dotenv').config();
+
+const { DB_HOST } = process.env;
+console.log(DB_HOST);
 
 mongoose.connect(DB_HOST, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Database connect success');
+    // console.log('Database connect success');
 })
     .catch(error => {
         console.log(error.message);
