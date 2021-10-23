fa797cd9c6107fae37560f22b4825175796ddd08
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-10-24 00:27:49 +0300

    schema

--- app.js
+++ app.js
@@ -13,13 +13,37 @@ const mongoose = require('mongoose');
 require ('dotenv').config();
 
 const { DB_HOST } = process.env;
-console.log(DB_HOST);
+// console.log(DB_HOST);
+
+const { Schema, model } = mongoose;
+
+const contactSchema = Schema({
+    name: String,
+    email: String,
+    phone: String,
+    favorite: Boolean
+});
+
+const Contact = model("contact", contactSchema);
+
+const newContact = {
+    name: 'Yuliia Bond',
+    email: '1213@gmail.com',
+    phone: '(066) 112-2333',
+    favorite: true
+}
 
 mongoose.connect(DB_HOST, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
-    // console.log('Database connect success');
+}).then(async () => {
+    try {
+        // console.log('Database connect success');
+        const result = await Contact.create(newContact);
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 })
     .catch(error => {
         console.log(error.message);
